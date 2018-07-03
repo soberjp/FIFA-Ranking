84cdeaf1006f32f688e1463b8fd1b3e92592dc2b
sober <38937176+soberjp@users.noreply.github.com>
2018-07-03 23:01:03 +0900

    Update main.js

--- _resouce/main.js
+++ _resouce/main.js
@@ -3,10 +3,8 @@
 import {chart} from './js/chart.js';
 
 // CSS一式を読み込んでパッケージ
-// require('ress/ress.css');
 require('materialize-css/dist/css/materialize.css');
 require('c3/c3.css');
-// require('./css/icon.css');
 require('./css/stylesheet.css');
 
 // materialize(JS)を読み込んでパッケージ
